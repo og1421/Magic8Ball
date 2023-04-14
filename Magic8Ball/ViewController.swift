267bf7f63144eca45eeb0b00b09a2c2f06af72f1
Orlando Moraes Martins <orlandomoraesmartins@gmail.com>
2023-04-14 16:30:48 -0300

    Making show random image

--- Magic8Ball/ViewController.swift
+++ Magic8Ball/ViewController.swift
@@ -9,11 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var askButtom: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        imageView.image = UIImage(imageLiteralResourceName: "ball3")
     }
 
-
+    @IBAction func askButtonPressed(_ sender: Any) {
+        let imageSelected = Int.random(in: 1...5)
+        
+        imageView.image = UIImage(imageLiteralResourceName: "ball\(imageSelected)")
+    }
+    
 }
 
